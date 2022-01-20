841bfd97323c417ed18ee1ad1c3a2d35ffd71e56
Andrew Dias <andrew.1995@outlook.com>
2022-01-19 23:44:14 -0300

    initial commit

---
+++ lib/domains/usecases/authentication.dart
@@ -0,0 +1,6 @@
+import 'package:meta/meta.dart';
+import '../entities/entities.dart';
+
+abstract class Authentication {
+  Future<AccountEntity> auth({@required String email, @required String password}); 
+}
